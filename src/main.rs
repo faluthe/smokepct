76bde32e291c6c3016fb69944cd04d62ec884a49
faluthe <faluninja@gmail.com>
2022-11-03 23:30:03 -0400

    working update

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,44 @@
-use std::{fs::File, io::Read};
+use std::{fs::File, io::Read, collections::HashSet};
 
-fn main() {
+use b2sum_rust::Blake2bSum;
+
+fn dump_manifest() -> HashSet<String> {
     let mut manifest = File::open("MANIFEST.txt").expect("Manifest not found");
     let mut data = String::new();
     manifest.read_to_string(&mut data).unwrap();
-    let data = data.replace("\r\n", "");
-    let mut sums: Vec<String> = Vec::new();
+    let mut sums: HashSet<String> = HashSet::new();
     
-    let mut i = 0;
-    while i < data.len() {
-        let s = data.chars().skip(i).take(32).collect();
-        println!("{s}");
-        sums.push(s);
-        i += 32;
+    for l in data.lines() {
+        sums.insert(l.to_uppercase());
+    }
+    sums
+}
+
+fn permute(mut k: usize, mut s: Vec<char>) -> String {
+    for i in 1..s.len() {
+        s.swap(k % (i + 1), i);
+        k = k / (i + 1);
+    }
+    s.into_iter().collect()
+}
+
+fn factorial(x: usize) -> usize {
+    if x == 1 {
+        x
+    } else {
+        x * factorial(x - 1)
+    }
+}
+
+fn main() {
+    let sums = dump_manifest();
+    let testkey = "FJMNPRTX";
+    let b2b = Blake2bSum::new(64);
+    for k in 0..factorial(testkey.len()) {
+        let x = permute(k, testkey.chars().collect());
+        let check = b2b.read_str(x.clone() + "\n");
+        if sums.contains(&check) {
+            println!("Found solution: {}", x);
+        }
     }
 }
