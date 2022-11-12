3a596503bbef5d1d13d822f09dc4f153fcc54029
danmuck <danmuck426@gmail.com>
2022-11-11 22:18:05 -0500

    added log files and some minor changes to init data

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
-use std::{fs::File, io::Read, collections::HashSet, thread, time::{Instant, Duration}};
+use std::{fs::File, io::{Read, Write}, collections::HashSet, thread, time::{Instant}};
 use b2sum_rust::Blake2bSum;
+
+// Initial Data
+const THREADS: usize = 8;
+const PZL_KEY: &str = "ABCDEFGHIJK";
+const MAN_FILE: &str = "1";
 
 
 fn dump_manifest() -> HashSet<String> {
-    let manifest_path = "MANIFEST/".to_owned() + "1";
+    let manifest_path = "MANIFEST/".to_owned() + MAN_FILE;
     let mut manifest = File::open(manifest_path).expect("Manifest not found");
     let mut data = String::new();
     manifest.read_to_string(&mut data).unwrap();
@@ -32,28 +37,27 @@ fn factorial(x: usize) -> usize {
 }
 
 fn main() {
-    let testkey = "HFXCWEAUTN";
-    let max_permutations = factorial(testkey.len());
-    println!("max: {}", max_permutations);
+    let max_permutations = factorial(PZL_KEY.len());
+    println!("[ pct{} :: {} ]", MAN_FILE, PZL_KEY);
+    println!("base: {}", PZL_KEY.chars().count());
+    println!("max: {} \n", max_permutations);
     let start = Instant::now();
     let mut threads = vec![];
-    let thread_count = 8;
 
-    
-    for t in 0..thread_count {
+    for t in 0..THREADS {
         let sums = dump_manifest();
-        let tmp_key = testkey;
+        let tmp_key = PZL_KEY;
         let b2b = Blake2bSum::new(64);
         println!("thread: ({}) for {}", t, tmp_key);
 
-        let block = &max_permutations / thread_count;
+        let block = &max_permutations / THREADS;
         let max = block + (block * t);
         let min = max - block;
 
         println!("\tmin: {}\tmax: {}", min, max);
         let thread_block = thread::spawn(move || { 
             for k in min..max {
-                let x = permute(k, testkey.chars().collect());
+                let x = permute(k, PZL_KEY.chars().collect());
                 // println!("{}", x);
                 let check = b2b.read_str(x.clone() + "\n");
 
@@ -70,6 +74,17 @@ fn main() {
         let _ = thread.join().unwrap();
     }
 
-    println!(" . . ");
-    print!("time: {}", start.elapsed().as_secs());
+    println!("\n . . \n");
+    
+    println!(" [ pct{} :: {} ]", MAN_FILE, PZL_KEY);
+    println!("  . puzzle_base: {}", PZL_KEY.chars().count());
+    println!("  . iters: {}", max_permutations);
+    println!("  . threads: {}", THREADS);
+    println!("  . time: {}ms", start.elapsed().as_millis());
+
+    let mut log_file = File::options().append(true).create(true).open("logs/timers").expect("My dumbass error");
+
+    log_file.write( format!("{:9} {}\n{:9} {}\n{:9} {}\n{:9} {}ms\n\n", "Key:", PZL_KEY, "Threads:", THREADS, "Base:", PZL_KEY.chars().count(), "Time:", start.elapsed().as_millis()).as_bytes() ).unwrap();
+
+
 }
